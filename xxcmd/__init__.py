ebcc8fcac944561ec7e19160b30168571c3448d8
Graham King <grking.email@gmail.com>
2021-02-14 23:38:15 +0000

    Don't care if inspecting the version number fails.

--- xxcmd/__init__.py
+++ xxcmd/__init__.py
@@ -1,6 +1,6 @@
 """A helper for remembering useful shell commands."""
 import os
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 from .cmdmanager import CmdManager
 from .dbitem import DBItem
 from .main import main, __version__
@@ -10,7 +10,10 @@ from .main import main, __version__
 __dev_version__ = ''
 thisdir = os.path.dirname(os.path.realpath(__file__))
 if os.path.isdir(os.path.join(thisdir, os.pardir, '.git')):
-    ver = check_output('git describe --tags'.split())
+    try:
+        ver = check_output('git describe --tags'.split())
+    except CalledProcessError:
+        ver = ''  # don't care
     if ver:
         ver = ver.decode('utf-8').strip()
         __dev_version__ = ver
